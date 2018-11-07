454d416822da0fa61f90d026d0f21d1eaf527511
ChoWonmin <0108257@gmail.com>
2018-11-07 20:14:27 +0900

    generate sample data

--- src/data/genData.js
+++ src/data/genData.js
@@ -1,19 +1,64 @@
 const fs = require('fs');
+const _ = require('lodash');
 
-const file = './dblp-ref-3.json';
+const data = {};
 
-fs.readFile(file, function (err, data) {
-  const str = data.toString('utf8');
-  let copy = new Array(str.length + 1000);
+for (let i=0; i<300; i++) {
+  data[i] = {
+    year: 2000,
+    references: [],
+    cluster: parseInt(Math.random()*5)
+  };
+}
 
-  for (let i=0; i<str.length; i++) {
-    copy[i] = str[i];
-    if (str[i++]==='}')
-      copy[i] = ',';
+for (let i=5; i<300; i++) {
+  if (i<10)
+    data[i]['year'] = 2001;
+  else if (i<15)
+    data[i]['year'] = 2002;
+  else if (i<22)
+    data[i]['year'] = 2003;
+  else if (i<30)
+    data[i]['year'] = 2004;
+  else if (i<40)
+    data[i]['year'] = 2005;
+  else if (i<50)
+    data[i]['year'] = 2006;
+  else if (i<70)
+    data[i]['year'] = 2007;
+  else if (i<95)
+    data[i]['year'] = 2008;
+  else if (i<110)
+    data[i]['year'] = 2009;
+  else if (i<130)
+    data[i]['year'] = 2010;
+  else if (i<155)
+    data[i]['year'] = 2011;
+  else if (i<190)
+    data[i]['year'] = 2012;
+  else if (i<235)
+    data[i]['year'] = 2013;
+  else if (i<280)
+    data[i]['year'] = 2014;
+  else if (i<310)
+    data[i]['year'] = 2015;
+  else if (i<350)
+    data[i]['year'] = 2016;
+}
+
+_.forEach(data, (e, i) => {
+  const len = parseInt(i/15)+1;
+  for (let j=0; j<len; j++) {
+    let value = i - parseInt(Math.random()*i);
+    let isFind = _.find(e['references'], e => e===value);
+
+    if (!isFind) {
+      e['references'].push(value);
+    }
   }
+});
 
-  fs.writeFile('./out.json', copy, function (err) {
+fs.writeFile('./out.json', JSON.stringify(data), function (err) {
     if (err) throw err;
     console.log('Saved!');
-  });
 });
