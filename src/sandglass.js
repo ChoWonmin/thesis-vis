99e8fdc7b3486fcab733205f1b40a2efa8216d94
ChoWonmin <0108257@gmail.com>
2018-12-04 00:59:56 +0900

    search list

--- src/sandglass.js
+++ src/sandglass.js
@@ -1,15 +1,26 @@
 const searchBar = document.getElementById('search-input');
+const searchList = document.getElementById('search-list');
+const searchItem = (title, authors) => `<div class="item">
+    <div class="title">${title}</div>
+    <div class="author">${authors}</div>
+</div>`;
+let data = {};
 
 searchBar.addEventListener('keyup', async function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
 
-      const data = (await axios.get('http://dblp.ourguide.xyz/papers/search',
+      data = (await axios.get('http://dblp.ourguide.xyz/papers/search',
         {
           params : {
             q: searchBar.value
-        }
-      })).data;
+          }
+        })).data;
+
+      searchList.innerHTML = '';
+      _.forEach(data, e => {
+        searchList.innerHTML += searchItem(e.title, e.authors);
+      });
 
       console.log(data);
 
