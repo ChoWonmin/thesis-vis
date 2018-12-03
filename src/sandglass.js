6f299fcde61c8dccb7ce1d6ca51ec48bb8e68139
ChoWonmin <0108257@gmail.com>
2018-12-03 22:53:17 +0900

    add axios

--- src/sandglass.js
+++ src/sandglass.js
@@ -1,8 +1,19 @@
 const searchBar = document.getElementById('search-input');
 
-searchBar.addEventListener('keyup', function(event) {
+searchBar.addEventListener('keyup', async function(event) {
     event.preventDefault();
     if (event.keyCode === 13) {
-      console.log(searchBar.value);
+
+      const data = (await axios.get('http://dblp.ourguide.xyz/papers/search',
+        {
+          params : {
+            q: searchBar.value
+        }
+      })).data;
+
+      console.log(data);
+
     }
 });
+
+
